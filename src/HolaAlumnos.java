320b948a49f623b74dee07877d44adb1a31f5105
KaruzoHikari <karuzohikari@pop-os.localdomain>
2021-09-23 18:26:22 +0200

    Mi primer commit

---
+++ src/HolaAlumnos.java
@@ -0,0 +1,11 @@
+public class HolaAlumnos {
+
+   /**
+    * Funcion main
+    *
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Hola alumnos");
+   }
+}
